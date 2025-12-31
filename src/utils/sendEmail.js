88f12b6a6e05f17c6f6ed1b8528d473138cd9e13
Wintorez7 <liordx76@gmail.com>
2025-12-31 20:54:03 +0530

    install resned package

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,12 +2,18 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, text) => {
-  await resend.emails.send({
-    from: "liordx76@gmail.com", // Resend default
-    to,
-    subject,
-    text,
-  });
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev", // Default sender
+      to,
+      subject,
+      text,
+    });
+    console.log("Email sent successfully!");
+  } catch (err) {
+    console.log("Email sending failed:", err);
+    throw err;
+  }
 };
 
 module.exports = sendEmail;
